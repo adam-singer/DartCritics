e16d95d0a06dab6963ff600e611ad7e6b78c941a
Lukas Renggli <renggli@gmail.com>
2012-12-31 17:31:03 +0100

    - fixed some imports

--- lib/critics.dart
+++ lib/critics.dart
@@ -4,10 +4,14 @@ library critics;
 
 import 'dart:io';
 
+import 'package:compiler_unsupported/compiler.dart';
+import 'package:compiler_unsupported/libraries.dart';
+
 import 'package:compiler_unsupported/implementation/mirrors/mirrors.dart';
-import 'package:compiler_unsupported/implementation/mirrors/mirrors_util.dart';
-import 'package:compiler_unsupported/implementation/mirrors/dart2js_mirror.dart';
-import 'package:compiler_unsupported/implementation/scanner/scannerlib.dart';
+ import 'package:compiler_unsupported/implementation/mirrors/mirrors_util.dart';
+
+// import 'package:compiler_unsupported/implementation/mirrors/dart2js_mirror.dart';
+// import 'package:compiler_unsupported/implementation/scanner/scannerlib.dart';
 
 part 'src/critics.dart';
 part 'src/rule.dart';
